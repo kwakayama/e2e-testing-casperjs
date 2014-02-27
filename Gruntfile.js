f195f2373f6c7af6c39596f47aef6c98b1ae880c
Kentaro Wakayama <wakayama.kentaro@gmail.com>
2014-02-27 07:37:23 +0100

    add grunt-casperjs

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,31 +24,33 @@ module.exports = function(grunt) {
       gruntfile: {
         src: 'Gruntfile.js'
       },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      }
-    },
-    nodeunit: {
-      files: ['test/**/*_test.js']
     },
     watch: {
       gruntfile: {
         files: '<%= jshint.gruntfile.src %>',
         tasks: ['jshint:gruntfile']
       },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'nodeunit']
+      test: {
+        files: ['tests/**/*.js'],
+        tasks: ['casperjs']
       }
-    }
+    },
+    casperjs: {
+      options: {
+        async: {
+            parallel: false
+        }
+      },
+      files: ['tests/casperjs/**/*.js']
+    },
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-casperjs');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+  grunt.registerTask('default', ['jshint']);
 
 };

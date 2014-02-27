c39d39c6d3f195d3e698c79a859f32c83d69dcd4
Kentaro Wakayama <wakayama.kentaro@gmail.com>
2014-02-27 08:18:26 +0100

    add basic testing

--- tests/casperjs/casper.js
+++ tests/casperjs/casper.js
@@ -1,19 +1,16 @@
-// googletesting.js
-casper.test.begin('Google search retrieves 10 or more results', 5, function suite(test) {
-    casper.start("http://www.google.fr/", function() {
-        test.assertTitle("Google", "google homepage title is the one expected");
-        test.assertExists('form[action="/search"]', "main form is found");
-        this.fill('form[action="/search"]', {
-            q: "casperjs"
-        }, true);
-    });
+casper.test.begin('Basic Setup Test', 3, function suite(test) {
 
-    casper.then(function() {
-        test.assertTitle("casperjs - Recherche Google", "google title is ok");
-        test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
-        test.assertEval(function() {
-            return __utils__.findAll("h3.r").length >= 10;
-        }, "google search for \"casperjs\" retrieves 10 or more results");
+    casper.start("index.html", function() {
+        this.echo(this.getCurrentUrl());
+
+        test.assertTitle("Backbone.js • TodoMVC", "title is the one expected");
+
+        test.assertTextExists('todos', 'page body contains header "todos"');
+
+        // check for the heading
+        test.assertEvalEquals(function() {
+            return __utils__.findOne('h1').textContent;
+        }, 'todos');
     });
 
     casper.run(function() {
